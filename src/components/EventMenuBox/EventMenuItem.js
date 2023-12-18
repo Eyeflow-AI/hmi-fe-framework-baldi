a645e391ccf58caf48c92bd29dd2d65dde98e9a5
Felipe <fas.felipesantos@gmail.com>
2023-12-18 13:39:39 -0300

    0.88.0

--- src/components/EventMenuBox/EventMenuItem.js
+++ src/components/EventMenuBox/EventMenuItem.js
@@ -1,103 +1,100 @@
 // React
-import React from 'react';
-
+import React from "react";
 
 //Design
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 //Internal
-import '../../css/animateFlicker.css';
+import "../../css/animateFlicker.css";
 
 //Third-party
 import { useTranslation } from "react-i18next";
-import { colors } from 'sdk-fe-eyeflow';
-import dateFormat from 'sdk-fe-eyeflow/functions/dateFormat';
-
+import { colors } from "sdk-fe-eyeflow";
+import dateFormat from "sdk-fe-eyeflow/functions/dateFormat";
 
 const style = {
   itemSx: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
     boxShadow: 2,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     borderRadius: 1,
-    cursor: 'pointer',
-    color: 'white',
-    textShadow: '1px 1px 2px #404040',
-    '&:hover': {
+    cursor: "pointer",
+    color: "white",
+    textShadow: "1px 1px 2px #404040",
+    "&:hover": {
       boxShadow: (theme) => `${theme.shadows[3]}, inset 0 0 0 2px black`,
     },
   },
   itemHeader: {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'space-between',
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
     paddingLeft: 1,
     paddingRight: 1,
   },
   itemImageBox: {
-    marginTop: -1
+    marginTop: -1,
   },
   itemImage: {
-    height: 70, width: "100%", filter: 'invert(1)'
+    height: 70,
+    width: "100%",
+    filter: "invert(1)",
   },
   itemFooter: {
     paddingBottom: 0.2,
   },
-}
+};
 
 style.selectedItemSx = Object.assign({}, style.itemSx, {
   boxShadow: (theme) => `${theme.shadows[2]}, inset 0 0 0 2px black`,
 });
 
-
 export default function EventMenuItem({
-  index
-  , dateField
-  , eventData
-  , selected
-  , onClick
-  , conveyorIcon
+  index,
+  dateField,
+  eventData,
+  selected,
+  onClick,
+  conveyorIcon,
 }) {
-
   const { t } = useTranslation();
 
   let thumbURL = eventData?.thumbURL ?? conveyorIcon;
-  let thumbStyle = Boolean(eventData.thumbStyle) ? eventData.thumbStyle : style.itemImage;
-  let status = eventData.status ?? '';
-  let eventTimeString = Boolean(eventData[dateField]) ? dateFormat(eventData[dateField]) : "";
-  let label = eventData.label ?? '';
+  let thumbStyle = Boolean(eventData.thumbStyle)
+    ? eventData.thumbStyle
+    : style.itemImage;
+  let status = eventData.status ?? "";
+  let eventTimeString = Boolean(eventData[dateField])
+    ? dateFormat(eventData[dateField])
+    : "";
+  let label = eventData.label ?? "";
 
   let boxStyle = Object.assign(
-    { backgroundColor: selected ? colors.statuses[status] : `${colors.statuses[status]}90` },
+    {
+      backgroundColor: selected
+        ? colors.statuses[status]
+        : `${colors.statuses[status]}90`,
+    },
     selected ? style.selectedItemSx : style.itemSx
   );
 
   return (
-    <Box
-      sx={boxStyle}
-      onClick={onClick}
-    >
+    <Box sx={boxStyle} onClick={onClick}>
       <Box sx={style.itemHeader}>
         <Box>
-          <Typography variant='h6'>
-            {index}
-          </Typography>
+          <Typography variant="h6">{index}</Typography>
         </Box>
         <Box display="flex" flexDirection="column" alignItems="end">
           <Box marginBottom={-1}>
-            <Typography variant='subtitle1'>
-              {label}
-            </Typography>
+            <Typography variant="subtitle1">{label}</Typography>
           </Box>
           <Box className={status === "running" ? "animate-flicker" : undefined}>
-            <Typography variant='subtitle2'>
-              {t(status)}
-            </Typography>
+            <Typography variant="subtitle2">{t(status)}</Typography>
           </Box>
         </Box>
       </Box>
@@ -109,10 +106,8 @@ export default function EventMenuItem({
       )}
 
       <Box sx={style.itemFooter}>
-        <Typography variant="subtitle2">
-          {eventTimeString}
-        </Typography>
+        <Typography variant="subtitle2">{eventTimeString}</Typography>
       </Box>
     </Box>
-  )
-};
+  );
+}
