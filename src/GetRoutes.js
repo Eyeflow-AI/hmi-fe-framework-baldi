7962d9702efabfbfee1ae926b5080fc6a78fa87f
Felipe <fas.felipesantos@gmail.com>
2024-01-22 12:08:38 -0300

    history page deleted, defaultValue in queryField in History

--- src/GetRoutes.js
+++ src/GetRoutes.js
@@ -1,8 +1,8 @@
-import React, { useMemo, lazy } from 'react';
+import React, { useMemo, lazy } from "react";
 
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
 
-import updatePath from './utils/functions/updatePath';
+import updatePath from "./utils/functions/updatePath";
 
 const Menu = lazy(() => import("./pages/Menu"));
 const Login = lazy(() => import("./pages/Login"));
@@ -10,7 +10,6 @@ const MonitorBatch = lazy(() => import("./pages/MonitorBatch"));
 const MonitorSerial = lazy(() => import("./pages/MonitorSerial"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Management = lazy(() => import("./pages/Management"));
-const History = lazy(() => import("./pages/History"));
 const Report = lazy(() => import("./pages/Report"));
 
 const homeURL = "/app/:stationSlugLabel/home";
@@ -19,16 +18,14 @@ const Query = lazy(() => import("./toolsPages/Query"));
 const Monitor = lazy(() => import("./toolsPages/Monitor"));
 const ImagesCapturer = lazy(() => import("./toolsPages/ImagesCapturer"));
 const ImagesAnalyser = lazy(() => import("./toolsPages/ImagesAnalyser"));
-const ChecklistConnector = lazy(() => import("./toolsPages/ChecklistConnector"));
+const ChecklistConnector = lazy(() =>
+  import("./toolsPages/ChecklistConnector")
+);
 const AppParameters = lazy(() => import("./toolsPages/AppParameters"));
 
 function NotFound() {
-  return (
-    <>
-      404: Not Found
-    </>
-  )
-};
+  return <>404: Not Found</>;
+}
 
 const components = {
   Menu: (pageOptions) => <Menu pageOptions={pageOptions} />,
@@ -36,27 +33,25 @@ const components = {
   MonitorSerial: (pageOptions) => <MonitorSerial pageOptions={pageOptions} />,
   Dashboard: (pageOptions) => <Dashboard pageOptions={pageOptions} />,
   Management: (pageOptions) => <Management pageOptions={pageOptions} />,
-  History: (pageOptions) => <History pageOptions={pageOptions} />,
   Report: (pageOptions) => <Report pageOptions={pageOptions} />,
 
   Query: (pageOptions) => <Query pageOptions={pageOptions} />,
   Monitor: (pageOptions) => <Monitor pageOptions={pageOptions} />,
   ImagesCapturer: (pageOptions) => <ImagesCapturer pageOptions={pageOptions} />,
   ImagesAnalyser: (pageOptions) => <ImagesAnalyser pageOptions={pageOptions} />,
-  ChecklistConnector: (pageOptions) => <ChecklistConnector pageOptions={pageOptions} />,
-  AppParameters: (pageOptions) => <AppParameters pageOptions={pageOptions} />
+  ChecklistConnector: (pageOptions) => (
+    <ChecklistConnector pageOptions={pageOptions} />
+  ),
+  AppParameters: (pageOptions) => <AppParameters pageOptions={pageOptions} />,
 };
 
-
 export default function Routes({
-  station
-  , authenticated
+  station,
+  authenticated,
   // , hasUserManagementPermission
   // , hasCaptureImagesPermission
 }) {
-
   return useMemo(() => {
-
     let appRoutes = [];
     let updatedHomeURL = updatePath(homeURL, station);
 
@@ -68,30 +63,33 @@ export default function Routes({
         // console.log({ value })
         appRoutes.push({
           path: value.path,
-          element: components[value.component](value)
-        })
-      };
-    };
-    appRoutes.push({ path: '/app', element: <Navigate to={updatedHomeURL} /> });
+          element: components[value.component](value),
+        });
+      }
+    }
+    appRoutes.push({ path: "/app", element: <Navigate to={updatedHomeURL} /> });
 
     return [
       {
-        path: '/app',
+        path: "/app",
         element: authenticated ? <Outlet /> : <Navigate to="/login" />,
-        children: appRoutes
+        children: appRoutes,
       },
       {
-        path: '/',
+        path: "/",
         element: !authenticated ? <Outlet /> : <Navigate to={updatedHomeURL} />,
         children: [
-          { path: '/login', element: <Login /> },
-          { path: '/', element: <Navigate to="/login" /> },
+          { path: "/login", element: <Login /> },
+          { path: "/", element: <Navigate to="/login" /> },
         ],
       },
       {
-        path: '*',
+        path: "*",
         element: <NotFound />,
       },
-    ]
-  }, [station, authenticated, /*hasUserManagementPermission, hasCaptureImagesPermission*/]);
-};
+    ];
+  }, [
+    station,
+    authenticated /*hasUserManagementPermission, hasCaptureImagesPermission*/,
+  ]);
+}
