83e7456b9efa577dc902a3264c3169201db06d7e
Matheus Caetano Rocha <matheus.caetano@eyeflow.ai>
2024-01-26 17:32:53 -0300

    update image capture's image sizes and info boxes

--- src/toolsPages/ImagesCapturer/index.js
+++ src/toolsPages/ImagesCapturer/index.js
@@ -157,6 +157,7 @@ export default function ImagesCapturer({ pageOptions }) {
                       alignItems: 'center',
                       boxShadow: 1,
                       borderRadius: '1rem',
+                      padding: '1rem',
                       cursor: 'pointer',
                     }}
                     onClick={onOpenDialog(item)}
@@ -166,8 +167,10 @@ export default function ImagesCapturer({ pageOptions }) {
                       image={`${item?.full_url}?time=${clock}`}
                       style={{
                         objectFit: 'contain',
-                        width: `calc(${pageOptions?.options?.IMAGE_SIZES[String(imagesList.length)]} - 10rem)`,
-                        height: pageOptions?.options?.IMAGE_SIZES[String(imagesList.length)],
+                        //maxWidth: `calc(${pageOptions?.options?.IMAGE_SIZES[String(imagesList.length)]})`,
+                        minWidth: '2560px * 0.3',
+                        //maxHeight: pageOptions?.options?.IMAGE_SIZES[String(imagesList.length)],
+                        minHeigth: '1440px * 0.3',
                         display: 'block',
                         margin: 'auto',
                         paddingBottom: '.5rem',
