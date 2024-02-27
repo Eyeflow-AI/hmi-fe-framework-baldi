58bbf6a389042d01b0f9d55c9ac10716e18869d6
Adriane Santos <adriane.santos@eyeflow.ai>
2024-02-27 18:14:28 -0300

    ..

--- src/PrepareApp.js
+++ src/PrepareApp.js
@@ -12,7 +12,6 @@ import getPartsListThunk from './store/thunks/partsList';
 import addInterceptors from './api/addInterceptors';
 import getOriginalURLPath from './utils/functions/getOriginalURLPath';
 import AlertUpdater from './utils/Hooks/AlertUpdater';
-import CheckVersion from './utils/Hooks/CheckVersion';
 
 // Thirdy-party
 import { useDispatch, useSelector } from 'react-redux';
